f8a40cae8d9b62238c9c5d2bd113dc93fd52bfd4
Glowstik-YT <69128866+Glowstik-YT@users.noreply.github.com>
2021-01-08 21:54:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // This template was created by Glowstikk#6969
 // If you like this come and check out my yt at http://bit.ly/Glowstik
 // If you do not understand this and would like to come to my channel where I will be making a whole seires on this!
@@ -15,15 +16,16 @@ for(const file of commandFiles){
     const command = require(`./commands/${file}`);
 
     client.commands.set(command.name, command);
-} // In order for this to work you must make a folder called commands
+}// In order for this to work you must make a folder called commands
 
 client.once('ready', () => {
-    console.log('Bot is up');// this sends a message to you in the console when the bot is online
+    console.log('Bot is online');// this sends a message to you in the console when the bot is online
 
-client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot)return;
-        const args = message.content.slice(prefix.length).split(/ +/);
-        const command = args.shift().toLowerCase();
-});//this is the part that "stores" your commands and it runs them when the command is called
+    client.on('message', message => {
+        if (!message.content.startsWith(prefix) || message.author.bot)return;
+            const args = message.content.slice(prefix.length).split(/ +/);
+            const command = args.shift().toLowerCase();
+    });//this is the part that "stores" your commands and it runs them when the command is called
+});
 
-client.login('Bot token');//put your bot token here
+client.login('bot-token');//put your bot token here
